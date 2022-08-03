974485c3d21111efff9a1248b52eb3f5ba5d8f59
Jose David Suarez Cardona <josedavid4227@gmail.com>
2022-08-03 16:45:08 -0500

    app

--- src/main/java/com/edu/utp/misiontic2022/c2/reto4/App.java
+++ src/main/java/com/edu/utp/misiontic2022/c2/reto4/App.java
@@ -1,5 +1,10 @@
 package com.edu.utp.misiontic2022.c2.reto4;
 
+import java.sql.SQLException;
+
+import com.edu.utp.misiontic2022.c2.reto4.model.dao.ProyectoBancoDao;
+import com.edu.utp.misiontic2022.c2.reto4.view.ReportesView;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+     
+       
+
+            var r = new ReportesView();
+            var banco = "Conavi";
+            r.proyectosFinanciadosPorBanco(banco);
+
+            var limiteinf = 60_000d;
+            r.totalAdeudadoPorProyectosSuperioresALimite(limiteinf);
+
+           r.lideresQueMasGastan();
+
+
+
+
+
+
+
+      
     }
 }
